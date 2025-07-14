1c860b6ab4fada22345c2ea60538ffde217de9cb
Alejandro Parra <alejandroparral@gmail.com>
2025-07-14 15:31:44 -0500

    featyure lmstudio plugin (#3)
    fix issues found with pom.xml dependencies
    
    Co-authored-by: Alejandro Parra Lasso <alejandro.parralasso@xpo.com>

--- src/main/java/japl/java/plugin/eclipse/lm/Activator.java
+++ src/main/java/japl/java/plugin/eclipse/lm/Activator.java
@@ -9,7 +9,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "com.example.lmstudio"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "japl.java.plugin.eclipse.lm"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
